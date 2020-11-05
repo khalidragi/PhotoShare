954fc20c26ae3e3c83692f641541fc248b03dbfa
Khalid <khalid.ragi@gmail.com>
2020-11-05 06:01:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import { db, storage } from './firebase';
 
 function App() {
+  const inputRef = useRef();
+  const [images, setImages] = useState([]);
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const unsubscribe = db
+      .collection('images')
+      .orderBy('createdAt', 'desc')
+      .onSnapshot((snap) => {
+        let dbSnap = snap.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+        setImages(dbSnap);
+      });
+
+    return unsubscribe;
+  }, []);
+
+  const uploadImage = (e) => {
+    const file = e.target.files[0];
+
+    const storageRef = storage.ref(file.name);
+
+    storageRef.put(file).on(
+      'state_changed',
+      (snap) => {
+        let percent = (snap.bytesTransferred / snap.totalBytes) * 100;
+        setProgress(percent);
+      },
+      (err) => console.log(err),
+      async () => {
+        const url = await storageRef.getDownloadURL();
+        await db.collection('images').add({
+          url,
+          createdAt: Date.now(),
+        });
+        setProgress(0);
+        inputRef.current.value = '';
+      }
+    );
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Firebase Storage</h1>
+        <input type="file" onChange={uploadImage} ref={inputRef} />
+        <progress value={progress} max="100"></progress>
+        <div className="photo-grid">
+          {images &&
+            images.map((image) => (
+              <img src={image.url} alt="" key={image.id} />
+            ))}
+        </div>
       </header>
     </div>
   );
